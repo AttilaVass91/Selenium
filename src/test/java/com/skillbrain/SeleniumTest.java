c45fff6e9c53a271e111001894632d9c5fcc5134
Attila Vass <40199200+AttilaVass@users.noreply.github.com>
2024-12-19 21:22:36 +0200

    primul meu test Login

--- src/test/java/com/skillbrain/SeleniumTest.java
+++ src/test/java/com/skillbrain/SeleniumTest.java
@@ -1,13 +1,14 @@
 package com.skillbrain;
 
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
 
-/**
- * Unit test for simple App.
- */
+
 public class SeleniumTest {
     public static void main(String[] args) {
         // Configurarea WebDriver-ului
@@ -21,14 +22,39 @@ public class SeleniumTest {
         WebDriver driver = new ChromeDriver(options);
         driver.get("https://the-internet.herokuapp.com/");
 
+        // Verificarea titlului paginii
+        String tabTitle = driver.getTitle();
+        System.out.println("Titlul tabului paginii este: " + tabTitle);
+
+        // Gasirea unui element de pe pagina
+        WebElement pageTitle = driver.findElement(By.cssSelector("#content h1"));
+        System.out.println("Titlul continutului paginii este: " + pageTitle.getText());
+
+        WebElement pageSubtitleTitle = driver.findElement(By.cssSelector("#content h2"));
+        System.out.println("Subtitlul continutului paginii este: " + pageSubtitleTitle.getText());
+
+        // Form authentication
+        WebElement formAuthLink = driver.findElement(By.linkText("Form Authentication"));
         try {
-            Thread.sleep(3000);
+            Thread.sleep(1000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        formAuthLink.click();
+
+        String actualTitle = driver.findElement(By.cssSelector("#content h2")).getText();
+        String expectedTitle = "Login Pages";
+        Assert.assertEquals(actualTitle,expectedTitle, "Titlul nu este cel asteptat");
+
+
+
+        try {
+            Thread.sleep(2000);
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        String pageTitle = driver.getTitle();
-        System.out.println("Titlul paginii este: " + pageTitle);
+
         // Inchiderea browserului
         driver.quit();
     }
